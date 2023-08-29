302776f210dbcb66b83d27f80a7d19e939f491c2
Georgypython <nov_georgy@inbox.ru>
2023-08-29 10:59:16 +0400

    change sittings

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k(q2ifr9co05e%k$kl4v53hng@t0erxr2d2e#mouv*x4lo6k^m'
+SECRET_KEY = os.getenv('SECRET_KEY',)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get("DEBUG", True))
 
