8d8f9e59119ac007306b05fcccb6e81d86161985
Georgypython <nov_georgy@inbox.ru>
2023-08-29 13:41:52 +0400

    change lang ru

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -121,7 +121,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 FIXTURE_DIRS = ['fixtures']
 AUTH_USER_MODEL = 'users.CustomUser'
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 
 TIME_ZONE = 'UTC'
